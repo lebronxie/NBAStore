39e32dcfc06e3a9643b5d4865816b419f21cbf2c
lebronxie <191155388@qq.com>
2019-01-10 17:32:29 +0800

    fix:单个删除按钮  和 删除选中 按钮 各是一个逻辑 和 php文件

--- NBAStore-v3/src/js/shoppingcart.js
+++ NBAStore-v3/src/js/shoppingcart.js
@@ -8,6 +8,7 @@ require(["./requirejs-config"], () => {
             dataType: "json",
             success: function(res) {
                 // console.log(res)
+                // 请求成功但是没有商品就显示如下页面信息
                 if (res.res_code === 1 && res.res_body.length == 0) {
                     $("#cart-box").html(`<div class="cart-empty">
                                             <h4>您的购物车是空的。</h4>
@@ -16,61 +17,75 @@ require(["./requirejs-config"], () => {
                                         </div>`)
                     return;
                 }
-                //如果请求成功并有购物车有商品
+
+                //请求成功并有购物车有商品
                 if (res.res_code === 1 && res.res_body.length > 0) {
-                    let html = template("cart-template", { cartList: res.res_body });
+                    //用模板渲染购物车页面 
+                    let html = template("cart-template", {
+                        cartList: res.res_body
+                    });
                     // console.log(html)
                     $("#cart-box").html(html)
 
 
                     //写购物车逻辑  单选 全选 计算总价 修改数据库等。。。
+                    
+                    //全选按钮点击事件
                     $("#all-btn").on("click", function() {
+                        //全选按钮改变状态
                         $(this).children().eq(0).toggleClass("on");
-                        // 全选
+                        // 全选 商品列表
                         $("#cart-box .checkbox").prop("checked", $(this).children().eq(0).hasClass("on")).toggleClass("on");
-                        count();
+                        count();//计算总价
                     })
+
                     //点击顶部删除按钮 删除选中的商品
                     $("#del-checked").on("click", function() {
+                        //判断是否有商品被选中
                         if ($("#cart-box .checkbox:checked").length > 0) {
                             if (confirm("确定删除选中商品吗？")) {
                                 //将要删除的商品id 打包以数组的方式发给后台 
                                 let delArr = [];
                                 $.each($("#cart-box .checkbox:checked"), function(i, item) {
-                                     let proId = $(item).parent().find(".proId").html();
-                                     delArr.push(proId);
+                                    let proId = $(item).parent().find(".proId").html();
+                                    delArr.push(proId);
                                 })
-                                     // console.log(delArr);
-                                    $.ajax({
-                                        data: {
-                                            delArr: delArr,
-                                        },
-                                        url: url.baseUrlPhp + "/project/nbastore/api/v1/delCart.php",
-                                        type: "post",
-                                        dataType: "json",
-                                        success: function(res) {
-                                            console.log(res)
-                                            if(res.res_code === 1){
-                                                 $("#cart").html(res.res_body.allNum);
-                                                 $("#cart-box .checkbox:checked").each(function(i,item){
-                                                    $(item).parent().parent().remove();
-                                                 })
-                                                let num = count(); //删除之后查页面还有多少条商品如果为0
-                                                if (num.length === 0) {
-                                                    location.reload(); //刷新页面
-                                                }
+                                // console.log(delArr);
+                                $.ajax({
+                                    data: {
+                                        delArr: delArr,
+                                    },
+                                    url: url.baseUrlPhp + "/project/nbastore/api/v1/delCarts.php",
+                                    type: "post",
+                                    dataType: "json",
+                                    success: function(res) {
+                                        console.log(res)
+                                        //删除数据库成功 前端删除相应商品行 更新商品总数和选中的商品总价 如果被全部删除完就判断 然后刷新页面
+                                        if (res.res_code === 1) {
+                                            $("#cart").html(res.res_body.allNum);
+                                            $("#cart-box .checkbox:checked").each(function(i, item) {
+                                                $(item).parent().parent().remove();
+                                            })
+                                            //删除之后查页面还有多少条商品如果为0 count函数会返回所有的checkbox jQuery对象 如果length =0 即代表购物车没有商品了
+                                            let num = count(); 
+                                            if (num.length === 0) {
+                                                location.reload(); //刷新页面
                                             }
-                                         
                                         }
-                                    })
+
+                                    }
+                                })
 
 
                             }
                         }
 
                     })
-                    //单个选择按钮点击事件
+
+
+                    //单个选择按钮点击事件 每点击一次都计算一次选中的总价
                     $("#cart-box .checkbox").on("click", function() {
+                        //判断是否全选
                         if ($("#cart-box .checkbox:checked").length === $("#cart-box .checkbox").length) {
                             $("#all-btn").children().eq(0).addClass("on");
                         } else {
@@ -78,25 +93,25 @@ require(["./requirejs-config"], () => {
                         }
 
                         //将勾选过的当前商品目录发送给后台 计算总价
-                        // console.log(count())
+                        
                         count();
 
 
                     })
-
+                    
+                    //定义一个计算当前选中商品的总价
                     function count() {
                         let count = 0;
                         //计算所有打钩 的 商品行 总价 
-
                         $.each($("#cart-box .checkbox:checked"), function(i, item) {
                             count += parseInt($(item).parent().parent().find(".colprice").html().replace("¥", ""));
                         })
                         $("#all-price").html("¥" + count + ".00");
-                        return $("#cart-box .checkbox")
+                        return $("#cart-box .checkbox"); //计算完成后返回当前页所有的商品 checkbox jQuery对象 留着有用
                     }
 
 
-                    //每一行的价格 数量 * 单价 
+                    //每一行的价格 数量 * 单价   每次改变商品数量都会重新计算 重新改变数据库
                     $(".number").on("change", function() {
                         //判断当前行有没有选择
 
@@ -162,67 +177,69 @@ require(["./requirejs-config"], () => {
                         }
                     })
 
-                   
-                  //点击支付按钮事件
-                  $("#payAll").on("click",function(){
+
+                    //点击支付按钮事件
+                    $("#payAll").on("click", function() {
                         // 判断是否登录
-                        if(!$.cookie("user")){
+                        if (!$.cookie("user")) {
                             alert("请先登录,登录后有优惠")
-                        }else{
-                             if ($("#cart-box .checkbox:checked").length > 0) {
-                           let allPayArr = [];
-                           // 将选中的商品传给数据库
-                          $.each($("#cart-box .checkbox:checked"),function(i,item){
-                            // console.log(res)
-                           let current_proId = $(item).parent().find(".proId").html();
-                           let current_number = $(item).parent().parent().find(".number").val();
-                            $.each(res.res_body,function(i,item){
-                                if(item["proid"] === current_proId){
-                                    //将当前行的数据添加到结算pay数据库 带上数量
-                                    item.number = current_number;
-                                    allPayArr.push(item)
-                                }
-                           })
-                          })
-                            //console.log(allPayArr);//将数组总的数据拼接成这样的字符串发送php文件
-                            let allPayStr = "insert into allPay (proid,title,picSrc,newPrice,oldPrice,size,number) values";
-                            let tempArr = [];
-                            $.each(allPayArr,function(i,item){
-                               let tempStr = `('${item.proid}','${item.title}','${item.picSrc}','${item.newPrice}','${item.oldPrice}','${item.size}','${item.number}')`;
-                               // console.log(tempStr);
-                               tempArr.push(tempStr);
-                            })
-                            // console.log(tempArr);
-                            console.log(tempArr.join(" ,"));
-                            let Str = tempArr.join(" ,");
-                            allPayStr += Str;
-                            // console.log(allPayStr);
-                            // "insert into allPay (proid,title,picSrc,newPrice,oldPrice,size,number) values 
-                            // ('$proId','$title','$picSrc','$newPrice','$oldPrice','$size','$number')"
-                             $.ajax({
-                                      type : "get",
-                                      dataType : "json",
-                                      url : url.baseUrlPhp + "/project/nbastore/api/v1/allPay.php",
-                                      data : {allPayStr:allPayStr},
-                                      success : function(res){
+                        } else {
+                            if ($("#cart-box .checkbox:checked").length > 0) {
+                                let allPayArr = [];
+                                // 将选中的商品传给数据库
+                                $.each($("#cart-box .checkbox:checked"), function(i, item) {
+                                    // console.log(res)
+                                    let current_proId = $(item).parent().find(".proId").html();
+                                    let current_number = $(item).parent().parent().find(".number").val();
+                                    $.each(res.res_body, function(i, item) {
+                                        if (item["proid"] === current_proId) {
+                                            //将当前行的数据添加到结算pay数据库 带上数量
+                                            item.number = current_number;
+                                            allPayArr.push(item)
+                                        }
+                                    })
+                                })
+                                //获取要支付的所有商品信息 传给后台 由于前端的后台技术不过硬 前端直接将sql 语句写好 传给后台 直接执行即可
+                                //console.log(allPayArr);//将数组总的数据拼接成这样的字符串发送php文件
+                                let allPayStr = "insert into allPay (proid,title,picSrc,newPrice,oldPrice,size,number) values";
+                                let tempArr = [];
+                                $.each(allPayArr, function(i, item) {
+                                    let tempStr = `('${item.proid}','${item.title}','${item.picSrc}','${item.newPrice}','${item.oldPrice}','${item.size}','${item.number}')`;
+                                    // console.log(tempStr);
+                                    tempArr.push(tempStr);
+                                })
+                                // console.log(tempArr);
+                                // console.log(tempArr.join(" ,"));
+                                let Str = tempArr.join(" ,");
+                                allPayStr += Str;
+                                // console.log(allPayStr);
+                                // "insert into allPay (proid,title,picSrc,newPrice,oldPrice,size,number) values 
+                                // ('$proId','$title','$picSrc','$newPrice','$oldPrice','$size','$number')"
+                                $.ajax({
+                                    type: "get",
+                                    dataType: "json",
+                                    url: url.baseUrlPhp + "/project/nbastore/api/v1/allPay.php",
+                                    data: {
+                                        allPayStr: allPayStr
+                                    },
+                                    success: function(res) {
                                         console.log(res);
-                                        window.location.href="/html/pay.html"
-                                      }
-                                   })
-                       }else{
-                            alert("请先选中商品再支付")
-                       }
+                                        window.location.href = "/html/pay.html"
+                                    }
+                                })
+                            } else {
+                                alert("请先选中商品再支付")
+                            }
                         }
-                        
-                      
-                     
-                  })
-                  
-                  
+
+
+
+                    })
+
 
 
                 }
-              
+
             }
         })
     })
